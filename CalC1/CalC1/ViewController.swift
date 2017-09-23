d8fbfd82dfd5d8af1b135e53ae3c94c5e7096803
Student <student@setass-mac-mini-9.local>
2017-09-23 11:34:03 -0400

    Basic UI for CalC1

---
+++ CalC1/CalC1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalC1
+//
+//  Created by Student on 2017-09-23.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
