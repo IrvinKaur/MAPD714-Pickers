fcc180ed8836223d43d1215ffb6799c7c2aa3300
Student <student@setass-mac-mini-9.local>
2017-10-11 20:06:45 -0400

    Pickers, Date scene

--- MAPD714 Pickers/MAPD714 Pickers/DatePickerViewController.swift
+++ MAPD714 Pickers/MAPD714 Pickers/DatePickerViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let date = NSDate()
+        datePicker.setDate(date as Date, animated: false)
 
         
     }
 
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let date = datePicker.date
+        print(date)
+        let message = "The Date and Time you selected is \(date)"
+        
+        let alert = UIAlertController(
+            title: "Date and Time selected",
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "That's so True!",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
   
 }
---
+++ MAPD714 Pickers/MAPD714 Pickers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714 Pickers
+//
+//  Created by Student on 2017-10-11.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
